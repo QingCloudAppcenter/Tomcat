d06e9a04c47808051ebc4e64139f419ad820e49b
Calvin Yu <calvinyu@yunify.com>
2017-07-06 15:24:53 +0800

    fix setupImage sh

--- scripts/setupImage.sh
+++ scripts/setupImage.sh
@@ -12,14 +12,18 @@ cd /opt/app-agent-linux-amd64/
 rm -rf agent.tar.gz
 cd /opt
 
+mkdir tomcat_app
+tar -xvf tomcat_app.tar.gz -C /opt/tomcat_app
+rm -rf tomcat_app.tar.gz
+
 echo "modify the max notify limit"
 mv /etc/sysctl.d/max-users-watch.conf /etc/sysctl.d/max-users-watch.conf_bak
-cp max-users-watch.conf /etc/sysctl.d
+cp /opt/tomcat_app/installpackages/max-users-watch.conf /etc/sysctl.d
 sysctl --system
 
 echo "modify ulimit"
 mv /etc/securitylimits.conf /etc/securitylimits.conf_bak
-cp limits.conf /etc/security
+cp /opt/tomcat_app/installpackages/limits.conf /etc/security
 
 echo "install zip unzip"
 yum -y install zip unzip
@@ -37,14 +41,6 @@ then
     echo "install openjdk"
     yum -y install java-1.8.0-openjdk
 
-    echo "install pyinotify"
-    tar -xvf pyinotify.tar.gz
-    cd /opt/pyinotify
-    python setup.py install
-    cd /opt
-    rm -rf pyinotify.tar.gz
-    rm -rf pyinotify/
-
     echo "install tomcat"
     curl -o tomcat.tar.gz http://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-7/v7.0.78/bin/apache-tomcat-7.0.78.tar.gz
     tar -xvf tomcat.tar.gz
@@ -56,9 +52,16 @@ then
     mkdir /tmp/war-listen
     mkdir /tmp/war-temp
     mkdir /root/.qingstor
-    mkdir tomcat_app
-    tar -xvf tomcat_app.tar.gz -C /opt/tomcat_app
-    rm -rf tomcat_app.tar.gz
+
+
+    echo "install pyinotify"
+    tar -xvf /opt/tomcat_app/installpackages/tomcat_node/pyinotify.tar.gz
+    cd /opt/tomcat_app/installpackages/tomcat_node/pyinotify
+    python setup.py install
+    cd /opt
+    rm -rf pyinotify.tar.gz
+    rm -rf pyinotify/
+    cp /opt/tomcat_app/installpackages/tomcat_node/tomcat_gc_log.conf /etc/rsyslog.d
     chmod +x /opt/tomcat_app/scripts/*.sh
     mv /opt/apache-tomcat-7.0.78/bin/catalina.sh /opt/apache-tomcat-7.0.78/bin/catalina.sh_bak
     cp /opt/tomcat_app/scripts/*.sh /opt/apache-tomcat-7.0.78/bin
@@ -87,9 +90,8 @@ fi
 if [ $node_type == "log"]
 then 
     cd /opt
-    mkdir tomcat_app
-    tar -xvf tomcat_app.tar.gz -C /opt/tomcat_app
-    cp /opt/tomcat_app/rsyslog.conf /etc/
+    mv /etc/rsyslog.conf /etc/rsyslog.conf_bak
+    cp /opt/tomcat_app/installpackages/log_node/rsyslog.conf /etc/
     cp /opt/tomcat_app/code/log_node/conf.d/*.toml /etc/confd/conf.d
     cp /opt/tomcat_app/code/log_node/template/*.tmpl /etc/confd/templates
     rm -rf /opt/tomcat_app
