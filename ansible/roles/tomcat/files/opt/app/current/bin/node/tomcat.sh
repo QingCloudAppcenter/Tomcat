4986992d1760fbd1ae9c6af3e4ba838582fe1374
Hongliang Wang <hongliangwang@yunify.com>
2020-04-25 21:57:06 +0800

    simplify confd templates

--- ansible/roles/tomcat/files/opt/app/current/bin/node/tomcat.sh
+++ ansible/roles/tomcat/files/opt/app/current/bin/node/tomcat.sh
@@ -2,11 +2,12 @@ initNode() {
   _initNode
   mkdir -p /data/tomcat/{war-listen,war-temp,webapps,logs}
   chown -R tomcat.svc /data/tomcat
+  ln -snf $JAVA_VERSION /opt/jdk/current
   ln -snf $TOMCAT_VERSION /opt/tomcat/current
   local defaultConfDir=/opt/tomcat/current/conf
   local confDir=/opt/app/current/conf/tomcat
   local fileName; for fileName in $(ls $defaultConfDir); do
-    [ -e "$confDir/$fileName" ] || ln -s $defaultConfDir/$fildName $confDir/$fileName
+    [ -e "$confDir/$fileName" ] || ln -s $defaultConfDir/$fileName $confDir/$fileName
   done
   ln -snf $confDir /data/tomcat/conf
   local htmlFile=/data/index.html; test -e "$htmlFile" || ln -s /opt/app/current/conf/caddy/index.html $htmlFile
