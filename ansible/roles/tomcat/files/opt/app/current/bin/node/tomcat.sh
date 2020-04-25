eba4baecb66c42df81a4a7b99da231897962537c
Hongliang Wang <hongliangwang@yunify.com>
2020-04-25 09:56:37 +0800

    add confd files

--- ansible/roles/tomcat/files/opt/app/current/bin/node/tomcat.sh
+++ ansible/roles/tomcat/files/opt/app/current/bin/node/tomcat.sh
@@ -2,5 +2,12 @@ initNode() {
   _initNode
   mkdir -p /data/tomcat/{war-listen,war-temp,webapps,logs}
   chown -R tomcat.svc /data/tomcat
+  ln -snf $TOMCAT_VERSION /opt/tomcat/current
+  local defaultConfDir=/opt/tomcat/current/conf
+  local confDir=/opt/app/current/conf/tomcat
+  local fileName; for fileName in $(ls $defaultConfDir); do
+    [ -e "$confDir/$fileName" ] || ln -s $defaultConfDir/$fildName $confDir/$fileName
+  done
+  ln -snf $confDir /data/tomcat/conf
   local htmlFile=/data/index.html; test -e "$htmlFile" || ln -s /opt/app/current/conf/caddy/index.html $htmlFile
 }
