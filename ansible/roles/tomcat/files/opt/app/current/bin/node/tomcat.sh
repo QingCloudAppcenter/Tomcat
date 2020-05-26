b9b03c1a663bae89cae9c926b00510804279937a
Hongliang Wang <hongliangwang@yunify.com>
2020-05-26 20:51:26 +0800

    add measure

--- ansible/roles/tomcat/files/opt/app/current/bin/node/tomcat.sh
+++ ansible/roles/tomcat/files/opt/app/current/bin/node/tomcat.sh
@@ -12,3 +12,29 @@ initNode() {
   ln -snf $confDir /data/tomcat/conf
   local htmlFile=/data/index.html; test -e "$htmlFile" || ln -s /opt/app/current/conf/caddy/index.html $htmlFile
 }
+
+measure() {
+  curl -m3 -sL -u "tomcat:$TOMCAT_PASSWORD" "localhost:8080/manager/status?XML=true" | 
+    grep -o "<connector.*</connector>" |
+    egrep -o "(name|currentThreadCount|currentThreadsBusy|maxTime|requestCount)=[^ >]*" |
+    sed 's/"//g; s/=/: /g; s/^name/connector/g; s/^/http_/g' |
+    yq r -j -
+}
+
+installWebApps() {
+  local webapps; webapps="$(echo $@ | jq -r '.webapps')"
+  local war; for war in ${webapps//,/ }; do
+    installWebApp $war
+  done
+}
+
+installWebApp() {
+  local targetPath=/data/tomcat/webapps/$1
+  if test -d $targetPath; then return 0; fi
+  sudo -u tomcat cp -r /opt/tomcat/current/webapps/$1 $targetPath
+}
+
+getFirstNode() {
+  local firstNode=${STABLE_NODES%% *}
+  echo -n ${firstNode##*/} | jq -Rsc '{labels: ["ip"], data: [[.]]}'
+}
